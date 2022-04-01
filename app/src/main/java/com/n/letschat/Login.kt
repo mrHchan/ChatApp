a29eff98c6627c24c30d9622cbdbdd4efac08e09
Heriyanto <buibui_chen@yahoo.com>
2022-04-01 22:45:50 +1030

    SignUp and Login set.

--- app/src/main/java/com/n/letschat/Login.kt
+++ app/src/main/java/com/n/letschat/Login.kt
@@ -1,11 +1,14 @@
 package com.n.letschat
 
+import android.R.attr
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
 
 class Login : AppCompatActivity() {
 
@@ -13,9 +16,14 @@ class Login : AppCompatActivity() {
     private lateinit var edtPassword: EditText
     private lateinit var btnLogin: Button
     private lateinit var btnSignUp: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
@@ -27,6 +35,28 @@ class Login : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email, password);
+        }
+
+    }
+
+    private fun login(email: String, password: String){
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent( this@Login, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                   Toast.makeText(this@Login, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
 
     }
 }
--- app/src/main/java/com/n/letschat/SignUp.kt
+++ app/src/main/java/com/n/letschat/SignUp.kt
@@ -1,11 +1,58 @@
 package com.n.letschat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
+import android.content.Intent
 import android.os.Bundle
+import android.service.controls.ControlsProviderService.TAG
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
 
 class SignUp : AppCompatActivity() {
+
+    private  lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignUp = findViewById(R.id.btn_signup)
+
+        btnSignUp.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            signUp(email, password)
+
+            }
+        }
+         private fun signUp(email: String, password: String){
+             mAuth.createUserWithEmailAndPassword(email, password)
+                 .addOnCompleteListener(
+                     this
+                 ) { task ->
+                     if (task.isSuccessful) {
+                         val intent = Intent( this@SignUp, MainActivity::class.java)
+                         startActivity(intent)
+                     } else {
+                        Toast.makeText(this@SignUp, "Error occurred, Please check again!", Toast.LENGTH_SHORT).show()
+                     }
+                 }
+         }
     }
-}
+
